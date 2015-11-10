597b260597f84435dc2628b6c9e0eb390515dae5
Software Engineer <software-engineering@users.noreply.github.com>
2015-11-10 21:38:56 +0100

    fix promise
    Task not waiting if everything is renderst with dust.render. Get also problems with grunt template <% dusthtml...%>

--- tasks/grunt-dust-html.js
+++ tasks/grunt-dust-html.js
@@ -18,10 +18,10 @@ module.exports = function(grunt) {
     var dfds = [];
 
     this.files.forEach(function(file) {
-      dfds = file.src.map(function(filepath) {
+      file.src.map(function(filepath) {
         var input = grunt.file.read(filepath);
 
-        return new Promise(function(resolve, reject) {
+        dfds.push(new Promise(function(resolve, reject) {
           dusthtml.render(input, opts, function(err, html) {
             if(err) {
               return reject(err);
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
             grunt.log.ok('File "' + file.dest + '" created.');
             resolve();
           });
-        });
+        }));
       });
     });
 
